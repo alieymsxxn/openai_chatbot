72eea65e021a16263a66a2b283f4ff719fc2c6c6
Mohsin Ali <mohsinalisep@gmail.com>
2024-12-01 20:32:09 +0500

    Added documentation as docstrings

--- src/app/chatbot/forms.py
+++ src/app/chatbot/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Length
 
+
 class CreateUpdatePromptForm(FlaskForm):
+    '''Form for creating or updating a prompt.
+
+    This form includes a single field for the prompt text, which is required
+    and must be at least one character long.
+    '''
     prompt = StringField('Prompt', validators=[DataRequired(), Length(min=1)])
